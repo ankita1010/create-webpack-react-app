3a5db1ed6c6d34c881376a6130ee48c00c016edb
Ankita <ankita.chakraborty4@wipro.com>
2019-11-02 18:16:34 +0530

    Steps completed: 1. added chalk messages, 2. created root directory for project, 3. created index.html file'

--- cli.js
+++ cli.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
-
-
-const [,, ...args] = process.argv;
-console.log(`Hello ${args}! How you doin?`)
+const chalk = require('chalk');
+const fs = require('fs');
+const HTML_TEMPLATE = require('./templates/indexHTML');
+const [, , ...args] = process.argv;
+if (args.length) {
+    console.log(chalk.green(`You have entered a name ${args}`));
+    fs.mkdirSync('./' + args[0]);
+    fs.writeFileSync(`${args[0]}/index.html`,HTML_TEMPLATE)
+}
+else
+    console.log(chalk.red('Please enter a name'));
---
+++ templates/indexHTML.js
@@ -0,0 +1,16 @@
+module.exports = (
+    `<!DOCTYPE html> 
+        <html lang="en"> 
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="build/style.css" />
+        <title>Welcome to React App!</title>
+        </head>
+        <body>
+        <div id="root"/>
+        </body>
+        </html>
+    `
+)
