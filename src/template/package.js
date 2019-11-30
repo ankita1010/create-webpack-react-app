cda34309248c37b4e786352dac711f5f8342481a
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2019-11-30 13:03:46 +0530

    updated command

--- src/template/package.js
+++ src/template/package.js
@@ -5,8 +5,8 @@ module.exports = (projectName) => (
 		description: 'This project is created using react and webpack',
 		scripts: {
 			start: 'webpack-dev-server',
-			build: 'webpack -env=production',
-			devbuild: 'webpack -env=development'
+			build: 'webpack --env=production',
+			devbuild: 'webpack --env=development'
 		},
 		browserslist: {
 			"production": [
